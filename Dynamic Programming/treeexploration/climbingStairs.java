ea469851430e6bb55520d594c52d2f28bdc4f195
Renyxx <d.shubhangi1510@gmail.com>
2023-12-18 13:00:13 +0530

    added: climbStairs Leetcode (dyprog)

---
+++ Dynamic Programming/treeexploration/climbingStairs.java
@@ -0,0 +1,21 @@
+package treeexploration;
+
+import java.util.HashMap;
+
+public class climbingStairs {
+    public static int climbStairs(int n){
+        return climbStairs(n, new HashMap<>());
+    }
+
+    public static int climbStairs(int n, HashMap<Integer, Integer> memo){
+        if (n==0 || n==1) return 1;
+        if (memo.containsKey(n)) return memo.get(n);
+        int count = climbStairs(n-1, memo) + climbStairs(n-2, memo);
+        memo.put(n, count);
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(4));
+    }
+}
